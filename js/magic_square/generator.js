5c656ec95306b13f1da94b551b73a9c05fc9aafc
Xanthe <xanthe@example.com>
2017-07-03 18:31:11 +0700

    Skeleton of the singlyEvenMagicSquare function

--- js/magic_square/generator.js
+++ js/magic_square/generator.js
@@ -44,8 +44,15 @@ class MagicSquareGenerator {
    * @return {number[][]}
    */
   static _singlyEvenMagicSquare(order) {
-    //TODO
-    return [[1]];
+    const p = order / 2;
+    const p_squared = p * p;
+    const M = MagicSquareGenerator._oddMagicSquare(p);
+
+    var square = Utils.prepopulate2DArray(order, order, 0);
+
+    
+    
+    return square;
   }
 
 }
