570a9f85d2c276595f508123856dd163d7838f61
Xanthe <xanthe@example.com>
2017-07-03 13:37:26 +0700

    Adding the public interface for the validator

---
+++ js/magic_square/validator.js
@@ -0,0 +1,12 @@
+class MagicSquareValidator {
+  /**
+   * 
+   * @param {number[][]} array 
+   * @returns {bool}
+   */
+  static checkArrayIsMagicSquare(array) {
+    // TODO
+    return false;
+  }
+}
+
