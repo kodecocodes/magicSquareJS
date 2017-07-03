32067b8700efb8ea9db0e2234253665abbe0b91e
Will <will@example.com>
2017-07-03 17:31:05 +0700

    Adding the structure to the generator

--- js/magic_square/generator.js
+++ js/magic_square/generator.js
@@ -5,7 +5,47 @@ class MagicSquareGenerator {
    * @return {number[][]}
    */
   static generateMagicSquare(order) {
-    // TODO
-    return new Square(1);
+    if (order == 2 || order < 1) {
+      throw "A magic square doesn't exist for that order";
+    }
+    switch (order % 4) {
+      case 0:
+        return Square.fromArray(MagicSquareGenerator._doublyEvenMagicSquare(order));
+      case 2:
+        return Square.fromArray(MagicSquareGenerator._singlyEvenMagicSquare(order));
+      default:
+        return Square.fromArray(MagicSquareGenerator._oddMagicSquare(order));
+    }
   }
+
+  /**
+   * 
+   * @param {number} order 
+   * @return {number[][]}
+   */
+  static _oddMagicSquare(order) {
+    //TODO
+    return [[1]];
+  }
+
+  /**
+   * 
+   * @param {number} order
+   * @return {number[][]} 
+   */
+  static _doublyEvenMagicSquare(order) {
+    //TODO
+    return [[1]];
+  }
+
+  /**
+   * 
+   * @param {number} order 
+   * @return {number[][]}
+   */
+  static _singlyEvenMagicSquare(order) {
+    //TODO
+    return [[1]];
+  }
+
 }
