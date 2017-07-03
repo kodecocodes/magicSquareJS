883eb6ffceb92c9122e868884d0a15731f8b8a74
Zach <zach@example.com>
2017-07-03 15:21:00 +0700

    Adding methods to allow editing of the magic square

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+function enterEditPreMode() {
+  const parent = this.parentNode;
+  const validationLabel = document.getElementById("validation");
+  validationLabel.classList.add("hidden");
+  var textArea = document.createElement("textarea");
+  const content = this.textContent;
+  const rows = content.split(/\r\n|\r|\n/);
+  const colCount = Math.max(...rows.map(r => r.length));
+  textArea.textContent = content;
+  textArea.rows = rows.length;
+  textArea.cols = colCount;
+  textArea.addEventListener("blur", endEditPreMode);
+  textArea.className = "flex-item";
+  parent.replaceChild(textArea, this);
+  textArea.focus();
+}
+
+function endEditPreMode() {
+  const parent = this.parentElement;
+  var preElement = document.createElement("pre");
+  preElement.textContent = this.value;
+  preElement.id = "square-panel";
+  preElement.className = "flex-item";
+  preElement.addEventListener("click", enterEditPreMode);
+  parent.replaceChild(preElement, this);
+}
+
