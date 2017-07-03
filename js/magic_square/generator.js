371ec1ba1e929896a2a91e6a8f632ed9414d5279
Xanthe <xanthe@example.com>
2017-07-03 13:48:52 +0700

    Adding public interface for the generator

---
+++ js/magic_square/generator.js
@@ -0,0 +1,11 @@
+class MagicSquareGenerator {
+  /**
+   * 
+   * @param {number} order 
+   * @return {number[][]}
+   */
+  static generateMagicSquare(order) {
+    // TODO
+    return new Square(1);
+  }
+}
