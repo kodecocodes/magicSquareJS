49de6d7355a5b0e2890e671ba160e6772875cbde
Will <will@example.com>
2017-07-03 18:36:53 +0700

    Adding array generation utils

--- js/magic_square/utils.js
+++ js/magic_square/utils.js
@@ -1,3 +1,24 @@
 class Utils {
+  /**
+  * 
+  * @param {number} length 
+  * @param {number} value 
+  * @return {number[]}
+  */
+  static prepopulate1DArray(length, value) {
+    return Array.apply(null, Array(length))
+                .map(Number.prototype.valueOf, value)
+  }
 
+  /**
+   * 
+   * @param {number} rows 
+   * @param {number} columns 
+   * @param {number} value 
+   * @return {number[][]}
+   */
+  static prepopulate2DArray(rows, columns, value) {
+    return Array.apply(null, Array(rows))
+                .map(_ => Utils.prepopulate1DArray(columns, value));
+  }
 }
